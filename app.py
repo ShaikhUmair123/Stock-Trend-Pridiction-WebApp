5d2cfbe01d52b744e19899d9813d4be6378eb271
ShaikhUmair123 <92623065+ShaikhUmair123@users.noreply.github.com>
2021-10-16 14:59:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ data_training_array = scaler.fit_transform(data_training)
 
 # Load my model
 
-model = load_model('keras_model.h5')
+model = load_model('keras_model.pb')
 
 # Testing part
 past_100_days = data_training.tail(100)
